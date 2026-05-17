4202fd158b9a4360df230171caefa54d25386c17
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-05-17 21:35:44 +0530

    Main file

---
+++ backend/src/Main.java
@@ -0,0 +1,14 @@
+import model.Problem;
+
+public class Main {
+    public static void main(String[] args) {
+        Problem p1 = new Problem(
+            "Two Sum",
+            "Arrays",
+            "Easy",
+            "https://leetcode.com/problems/two-sum"
+        );
+
+        System.out.println(p1);
+    }
+}
