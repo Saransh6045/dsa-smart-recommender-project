52bef1e603341e9058869992802368b7d0bc7ae0
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-05-17 22:54:01 +0530

    Dataset working properly

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,15 +1,24 @@
 import model.Problem;
+import java.util.List;
 import data.ProblemRepository;
 
 public class Main {
     public static void main(String[] args) {
-        Problem p1 = new Problem(
+        //Main method to test the Problem class, how the problem is formed and if the constructor is working properly or not.
+        /*Problem p1 = new Problem(
             "Two Sum",
             "Arrays",
             "Easy",
             "https://leetcode.com/problems/two-sum"
         );
 
-        System.out.println(p1);
+        System.out.println(p1);*/
+
+        //Main method to test the ProblemRepository class, to check if the problems are being stored properly and if the getAllProblems method is working properly or not.
+        List<Problem> lst = ProblemRepository.getAllProblems();
+
+        for (Problem p : lst) {
+            System.out.println(p);
+        }
     }
 }
