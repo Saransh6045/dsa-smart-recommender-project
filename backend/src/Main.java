949809e9cdd4dccf0c4b16cfa0badee9c1b3d623
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-05-18 23:08:48 +0530

    Menu added

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+
 import model.Problem;
 import java.util.List;
 import data.ProblemRepository;
@@ -25,11 +26,24 @@ public class Main {
         }*/
 
         List<Problem> lst = ProblemRepository.getAllProblems();
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter topic: ");
-        String topic = br.readLine();
+
+        //Taking input of topics and difficulty level from the user to recommend problems based on those criteria.
+        System.out.println("Available Topics: \n 1. Arrays \n 2. Strings \n 3. Linked Lists \n 4. Recursion \n 5. Stacks \n 6. Queues");
+        System.out.println("Enter topic number: ");
+        int topicNum = Integer.parseInt(br.readLine());
+
+        String[] topics = {"Arrays", "Strings", "Linked List", "Recursion", "Stack", "Queue"};
+        String topic = topics[topicNum - 1];
+
+        System.out.println("Available Difficulty Levels: \n 1. Easy \n 2. Medium \n 3. Hard");
         System.out.println("Enter difficulty: ");
-        String difficulty = br.readLine();
+        int difficultyNum = Integer.parseInt(br.readLine());
+
+        String[] difficulties = {"Easy", "Medium", "Hard"};
+        String difficulty = difficulties[difficultyNum - 1];
+        
         List<Problem> recommendedList = service.RecommendationService.recommend(lst, topic, difficulty);
 
         System.out.println("Recommended Problems: ");
