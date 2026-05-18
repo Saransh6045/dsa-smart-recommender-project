199be2dabde5f37aec28ab98fd73ec9d375d8d98
Saransh Varshney <varshneysaransh6045@gmail.com>
2026-05-18 22:45:26 +0530

    Converted from Static to Interactive Recommender

--- backend/src/Main.java
+++ backend/src/Main.java
@@ -1,9 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import model.Problem;
 import java.util.List;
 import data.ProblemRepository;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Main method to test the Problem class, how the problem is formed and if the constructor is working properly or not.
         /*Problem p1 = new Problem(
             "Two Sum",
@@ -22,7 +25,12 @@ public class Main {
         }*/
 
         List<Problem> lst = ProblemRepository.getAllProblems();
-        List<Problem> recommendedList = service.RecommendationService.recommend(lst, "Arrays", "Easy");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter topic: ");
+        String topic = br.readLine();
+        System.out.println("Enter difficulty: ");
+        String difficulty = br.readLine();
+        List<Problem> recommendedList = service.RecommendationService.recommend(lst, topic, difficulty);
 
         System.out.println("Recommended Problems: ");
         for (Problem p : recommendedList) {
